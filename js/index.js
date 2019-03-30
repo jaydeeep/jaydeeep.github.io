947b1b19f80f7d7a805c13ca230766a1e75d9582
jaydeeep <joyadeeplimbu@gmail.com>
2019-03-30 11:53:17 +0545

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,12 +1,7 @@
 
     function images()
     {
-      let a=Math.floor((Math.random()*3)+1);
-      if (a==1) {
-        document.write('<img src=images/random/bhumbro.jpg>');
-      }
-      else if(a==2)
-        document.write("<img src=images/random/naiyo_jaana.jpg>");
-      else
-        document.write("<img src=images/random/nepal_flag.jpg>");
+      let imgs=['images/random/bhumbro.jpg','images/random/naiyo_jaana.jpg','images/random/nepal_flag.jpg','images/random/naiyo-jaana.jpg']
+      let a=Math.floor((Math.random()*imgs.length-1)+1);
+      document.write("<img src="+imgs[a]+">");
       }
