df0c08ff8e5e82c3923bd50365a9fd83276c5a65
jaydeeep <joyadeeplimbu@gmail.com>
2019-04-04 22:27:45 +0545

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
-
-    function images()
-    {
-      let imgs=['images/random/bhumbro.jpg','images/random/naiyo_jaana.jpg','images/random/naiyo-jaana.jpg'
+	let imgs=['images/random/bhumbro.jpg','images/random/naiyo_jaana.jpg','images/random/naiyo-jaana.jpg'
                 ,'images/random/concert.jpg','images/random/travel1.jpg','images/random/travel2.jpg','images/random/tu_hi_tha.jpg','images/random/tu_mil_gaya.jpg'];
       let a=Math.floor((Math.random()*imgs.length-1)+1);
+     function images()
+    {
       document.write("<img src="+imgs[a]+">");
-      }
+    }
+
+
+
